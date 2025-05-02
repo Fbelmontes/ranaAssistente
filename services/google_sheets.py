6834a203f29b3b3586d3cf85206b54a6d394c6f4
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-02 17:06:40 -0300

    at

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -15,10 +15,6 @@ def conectar_sheets():
 def obter_conteudo_salvo():
     sheet = conectar_sheets()
     return sheet.get_all_records()
-
-def salvar_transcricao(conteudo):
-    sheet = conectar_sheets()
-    sheet.append_row(["Reunião", conteudo])
     
 def salvar_na_planilha(url, conteudo):
     sheet = conectar_sheets()
@@ -39,21 +35,6 @@ def salvar_na_memoria(conteudo, nome_arquivo="Desconhecido"):
     aba.append_row([nome_arquivo, conteudo])
     return True
 
-def buscar_na_memoria(palavra_chave):
-    sheet = conectar_sheets()
-    dados = sheet.get_all_records()  # Pega todas as linhas da planilha
-
-    resultados = []
-    for registro in dados:
-        nome_arquivo = registro.get("URL", "")
-        conteudo = registro.get("Conteudo", "")
-        
-        # Verifica se a palavra chave está presente no conteúdo (ignorando maiúsculas/minúsculas)
-        if palavra_chave.lower() in conteudo.lower():  
-            resultados.append({"Nome do Arquivo": nome_arquivo, "Conteudo": conteudo})
-
-    return resultados
-
 def salvar_historico(pergunta, resposta):
 
     sheet = conectar_sheets()
@@ -65,19 +46,4 @@ def salvar_historico(pergunta, resposta):
 
     data_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     aba.append_row([data_hora, pergunta, resposta])
-
-def obter_historico(limit=5):
-    sheet = conectar_sheets()
-    aba = sheet.spreadsheet.worksheet("Histórico")
-    dados = aba.get_all_records()
-    return dados[-limit:]   
-
-def obter_ultimas_interacoes(qtd=3):
-    sheet = conectar_sheets()
-    try:
-        aba = sheet.spreadsheet.worksheet("Historico")
-        dados = aba.get_all_records()
-        return dados[-qtd:]
-    except:
-        return []
 
