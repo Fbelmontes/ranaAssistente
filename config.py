89bcb7df57bb0d7d23c3e2b57661de2c83854e8b
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-02 10:55:26 -0300

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 TEMA_PADRAO_ESCURO = False
 
 # API do OpenRouter
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "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"
 OPENROUTER_API_BASE = "https://openrouter.ai/api/v1"
 
 # Google Sheets
