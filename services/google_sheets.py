71f50e8092a78342cfd55b25951bb9dfae3dae27
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-02 22:37:55 -0300

    2

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -7,7 +7,8 @@ from config import SPREADSHEET_URL, CREDENTIALS_PATH
 
 def conectar_sheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_PATH, scope)
+    cred_dict = json.loads(st.secrets["GOOGLE_SHEETS_CREDENTIALS"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(cred_dict, scope)
     client = gspread.authorize(creds)
     return client.open_by_url(SPREADSHEET_URL)
 
