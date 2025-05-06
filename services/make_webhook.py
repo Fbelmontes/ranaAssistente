0a571f97b8e8a446717bb6a03cfdf40f2ae663e9
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-06 18:04:52 -0300

    2

--- services/make_webhook.py
+++ services/make_webhook.py
@@ -8,8 +8,14 @@ def enviar_evento_para_make(dados_evento):
         response = requests.post(url, json=dados_evento)
 
         if response.status_code == 200:
-            return {"sucesso": True, "resposta": response.json() if response.text else "OK"}
+            try:
+                json_resposta = response.json()
+            except Exception:
+                json_resposta = response.text or "OK"
+
+            return {"sucesso": True, "resposta": json_resposta}
         else:
             return {"sucesso": False, "erro": response.text}
+
     except Exception as e:
         return {"sucesso": False, "erro": str(e)}
