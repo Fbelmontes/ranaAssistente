223231b095b4d0377609a749b10d2d3c48f88e50
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-07 15:16:44 -0300

    2

--- services/hubspot_oauth.py
+++ services/hubspot_oauth.py
@@ -9,9 +9,9 @@ def trocar_code_por_token(code):
 
     data = {
         "grant_type": "authorization_code",
-        "client_id": os.getenv("HUBSPOT_CLIENT_ID"),
-        "client_secret": os.getenv("HUBSPOT_CLIENT_SECRET"),
-        "redirect_uri": os.getenv("HUBSPOT_REDIRECT_URI"),
+        "client_id": "b57c8418-dd11-4075-92bc-12489ee69f5d",
+        "client_secret": "152a8b5c-e5b9-4497-a086-bc22d40aac38",
+        "redirect_uri": "https://app.hubspot.com/oauth/authorize?client_id=b57c8418-dd11-4075-92bc-12489ee69f5d&redirect_uri=https://ranaassistente.streamlit.app/&scope=crm.objects.contacts.write%20crm.objects.marketing_events.read%20crm.objects.marketing_events.write%20oauth%20crm.objects.contacts.read",
         "code": code
     }
 
