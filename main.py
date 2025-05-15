f0558c1086ab45fd39ef2088aa48ac162df7c888
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2025-05-15 11:44:17 -0300

    2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from services.webscraping import buscar_informacoes, scraping_evento_component
 from services.google_sheets import salvar_na_planilha_2, conectar_sheets
 from components.enviar_evento_make import enviar_evento_make_component
 from components.linkedin_integration import linkedin_integration_component
-from components.consultar_eventos import obter_eventos_sales_navigator
+from components.consultar_eventos import consultar_eventos_component
 
 # Configuração da página
 st.set_page_config(page_title="RANA - Assistente", page_icon="🤖", layout="wide")
